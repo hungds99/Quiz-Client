5b97060cf0977fde85d5bbc200315a2ae1749dc9
Đinh Sỹ Hùng <34293141+hungds99@users.noreply.github.com>
2022-07-25 08:42:31 +0700

    :zad: gitignore fixed

--- src/pages/playQuiz/playerJoin/playerJoin.js
+++ src/pages/playQuiz/playerJoin/playerJoin.js
@@ -1,20 +1,21 @@
-import { Box, Button, makeStyles, TextField } from "@material-ui/core";
-import GraphicEqIcon from "@material-ui/icons/GraphicEq";
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { useHistory } from "react-router";
-import { RoutePath } from "../../../configs";
-import { NotiTypeEnum } from "../../../constants";
-import { UIActions } from "../../../redux/actions/uiActions";
-import HostServices from "../../../services/hostServices";
+import { Box, Button, makeStyles, TextField } from '@material-ui/core';
+import GraphicEqIcon from '@material-ui/icons/GraphicEq';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router';
+import { RoutePath } from '../../../configs';
+import { NotiTypeEnum } from '../../../constants';
+import { UIActions } from '../../../redux/actions/uiActions';
+import HostServices from '../../../services/hostServices';
+import { configSocket } from '../../../sockets/rootSocket';
 
 const useStyles = makeStyles(() => ({
   root: {
-    height: "100vh",
-    backgroundImage: `url(${"/static/images/backgrounds/child_river_dreams_127495_1366x768.jpg"})`,
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
+    height: '100vh',
+    backgroundImage: `url(${'/static/images/backgrounds/child_river_dreams_127495_1366x768.jpg'})`,
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
   },
 }));
 
@@ -29,49 +30,40 @@ const PlayerJoin = () => {
     setPin(e.target.value);
   };
 
+  useEffect(() => {
+    configSocket();
+  }, []);
+
   const handleJoinGame = async () => {
     if (pin) {
       const { data } = await HostServices.getByPin(pin);
       if (data && data.result && data.result.pin) {
-        history.push(RoutePath.player.lobby.replace(":hostId", data.result.id));
+        history.push(RoutePath.player.lobby.replace(':hostId', data.result.id));
       } else {
-        dispatch(
-          UIActions.showNotification(NotiTypeEnum.error, "Pin is not correct")
-        );
+        dispatch(UIActions.showNotification(NotiTypeEnum.error, 'Pin is not correct'));
       }
     }
   };
   return (
     <Box className={classes.root}>
-      <Box
-        display="flex"
-        flexDirection="column"
-        justifyContent="center"
-        alignItems="center"
-        paddingTop={15}
-      >
+      <Box display='flex' flexDirection='column' justifyContent='center' alignItems='center' paddingTop={15}>
         <Box
-          display="flex"
-          flexDirection="column"
-          justifyContent="center"
-          alignItems="center"
-          bgcolor="#ffffff"
+          display='flex'
+          flexDirection='column'
+          justifyContent='center'
+          alignItems='center'
+          bgcolor='#ffffff'
           padding={5}
           borderRadius={20}
         >
           <h2>Enter pin to join game</h2>
           <Box padding={2}>
-            <TextField
-              variant="outlined"
-              type="number"
-              onChange={handleChangePin}
-              defaultValue={pin}
-            />
+            <TextField variant='outlined' type='number' onChange={handleChangePin} defaultValue={pin} />
           </Box>
           <Box>
             <Button
-              variant="contained"
-              color="primary"
+              variant='contained'
+              color='primary'
               onClick={handleJoinGame}
               disableElevation
               startIcon={<GraphicEqIcon />}
--- src/pages/playQuiz/playerLobby/playerLobby.js
+++ src/pages/playQuiz/playerLobby/playerLobby.js
@@ -1,38 +1,38 @@
-import { Box, CircularProgress, makeStyles } from "@material-ui/core";
-import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
-import { useHistory, useParams } from "react-router";
-import { RoutePath } from "../../../configs";
-import HostSelectors from "../../../redux/selectors/hostSelectors";
-import { emitPlayerJoinRoom } from "../../../sockets/hostSockets";
-import { configSocket } from "../../../sockets/rootSocket";
+import { Box, CircularProgress, makeStyles } from '@material-ui/core';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useHistory, useParams } from 'react-router';
+import { RoutePath } from '../../../configs';
+import HostSelectors from '../../../redux/selectors/hostSelectors';
+import { emitPlayerJoinRoom } from '../../../sockets/hostSockets';
+import { configSocket } from '../../../sockets/rootSocket';
 
 const useStyles = makeStyles(() => ({
   root: {
-    height: "100vh",
-    backgroundImage: `url(${"/static/images/backgrounds/branch_birds_owls_scarf_94136_1366x768.jpg"})`,
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
+    height: '100vh',
+    backgroundImage: `url(${'/static/images/backgrounds/branch_birds_owls_scarf_94136_1366x768.jpg'})`,
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
   },
   counterContainer: {
-    height: "100%",
-    backgroundImage: `url(${"/static/images/backgrounds/landscape_art_road_127350_1366x768.jpg"})`,
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
+    height: '100%',
+    backgroundImage: `url(${'/static/images/backgrounds/landscape_art_road_127350_1366x768.jpg'})`,
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
   },
   counterBox: {
-    display: "flex",
-    height: "100%",
-    justifyContent: "center",
-    alignItems: "center",
+    display: 'flex',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   counter: {
     backgroundColor: `rgba(255, 255, 255, 0.5)`,
-    width: "100%",
-    textAlign: "center",
-    padding: "50px",
+    width: '100%',
+    textAlign: 'center',
+    padding: '50px',
   },
   counterText: {
     fontSize: 100,
@@ -46,27 +46,18 @@ function PlayerLobby() {
   const startCounter = useSelector(HostSelectors.selectStartCounter);
 
   useEffect(() => {
-    configSocket();
-    setTimeout(() => {
-      emitPlayerJoinRoom({ id: hostId });
-    }, 0);
+    emitPlayerJoinRoom({ id: hostId });
   }, [hostId]);
 
   useEffect(() => {
     if (startCounter) {
-      history.push(RoutePath.player.started.replace(":hostId", hostId));
+      history.push(RoutePath.player.started.replace(':hostId', hostId));
     }
   }, [startCounter, history, hostId]);
 
   return (
     <Box className={classes.root}>
-      <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        flexDirection="column"
-        paddingTop={15}
-      >
+      <Box display='flex' justifyContent='center' alignItems='center' flexDirection='column' paddingTop={15}>
         <h2>Waiting Join....</h2>
         <Box paddingTop={5}>
           <CircularProgress />
